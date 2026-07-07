0e4716270cfbe8835486b764fa481264cd4bf27f
GPuebla <gabrielpuebla16@gmail.com>
2026-07-07 07:55:43 -0300

    feat: implement routing for lesson controllers and refactor server setup

--- server.js
+++ server.js
@@ -1,17 +1,8 @@
 const express = require('express');
 const app = express();
+const routes = require('./routes/index');
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
-});
-
-app.get('/welcome', (req, res) => {
-  res.send('Welcome to our app!');
-});
-
-app.get('/goodbye', (req, res) => {
-  res.send('Goodbye!');
-});
+app.use('/', routes);
 
 const PORT = process.env.PORT || 3000;
 
