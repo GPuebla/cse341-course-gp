30be755a75d895d8ac179fd81f93d08ff38c01cd
GPuebla <gabrielpuebla16@gmail.com>
2026-07-13 21:42:52 -0300

    feat: enhance Swagger setup to dynamically configure host and schemes

--- p1/server.js
+++ p1/server.js
@@ -3,13 +3,21 @@ const swaggerUi = require('swagger-ui-express');
 
 const mongodb = require('./data/db.js');
 const app = express();
+app.set('trust proxy', 1);
 app.use(express.json());
 const routes = require('./routes/index');
 
 try {
   const swaggerDocument = require('./swagger.json');
-  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-  app.get('/swagger.json', (req, res) => res.json(swaggerDocument));
+  const getSwaggerDocument = (req) => ({
+    ...swaggerDocument,
+    host: req.get('host'),
+    schemes: [req.protocol],
+  });
+  app.use('/api-docs', swaggerUi.serve, (req, res, next) =>
+    swaggerUi.setup(getSwaggerDocument(req))(req, res, next)
+  );
+  app.get('/swagger.json', (req, res) => res.json(getSwaggerDocument(req)));
 } catch (err) {
   console.log('swagger.json not found, run "npm run swagger" to generate it. /api-docs will not be available.');
 }
