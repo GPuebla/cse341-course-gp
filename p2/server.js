7f276b8810ebfc47fee0654ed3c52a0bd24912eb
GPUEBLA <gabrielpuebla16gmail.com>
2026-07-20 15:24:09 -0300

    feat: creating project 2

--- p2/server.js
+++ p2/server.js
@@ -3,13 +3,29 @@ const swaggerUi = require('swagger-ui-express');
 
 const mongodb = require('./data/db.js');
 const app = express();
+
+app.set('trust proxy', 1);
 app.use(express.json());
 const routes = require('./routes/index');
 
 try {
+   // Load the Swagger file.
   const swaggerDocument = require('./swagger.json');
-  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-  app.get('/swagger.json', (req, res) => res.json(swaggerDocument));
+
+  // Update host and protocol for the current request.
+  const getSwaggerDocument = (req) => ({
+    ...swaggerDocument,
+    host: req.get('host'),
+    schemes: [req.protocol],
+  });
+
+  // Serve Swagger UI with the updated configuration.
+  app.use('/api-docs', swaggerUi.serve, (req, res, next) =>
+    swaggerUi.setup(getSwaggerDocument(req))(req, res, next)
+  );
+
+  // Return the updated Swagger JSON.
+  app.get('/swagger.json', (req, res) => res.json(getSwaggerDocument(req)));
 } catch (err) {
   console.log('swagger.json not found, run "npm run swagger" to generate it. /api-docs will not be available.');
 }
