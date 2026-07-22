230428aea80ed963ecd68d2397c79877a1337c9a
GPUEBLA <gabrielpuebla16gmail.com>
2026-07-22 11:28:51 -0300

    feat: add Swagger documentation for welcome route

--- p2/routes/index.js
+++ p2/routes/index.js
@@ -1,6 +1,9 @@
 const routes = require('express').Router();
 
-routes.get('/', (req, res) => {
+routes.get('/', /*
+  #swagger.tags = ['Welcome']
+  #swagger.description = 'Welcome route / health check for the API.'
+*/ (req, res) => {
   res.send('Welcome to the Books API!');
 });
 
